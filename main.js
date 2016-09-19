c2781b17fa8c27811aa1b5f579dccd66419575b1
Joseph Daniel Winters <jdwinters96@gmail.com>
2016-09-18 22:49:50 -0400

    Began work on pause screen; no way to pause yet

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
 
 /* GLOBAL VARIABLES */
 
+// Global
 const CURRENT_SCORE = document.getElementById("currentscore");
 const HIGH_SCORE = document.getElementById("highscore");
 const HIGH_SCORE_STRING = "highscore";
@@ -15,6 +16,7 @@ const CONTEXT = CANVAS.getContext("2d");
 const REFRESH_RATE = 20;            // milliseconds between refreshes
 const BACKGROUND_COLOUR = "#729fcf";
 
+// Bees
 const BEE_UPDATED = "Successfully updated the bee.";
 const BEE_DEAD_AND_FALLEN = "The bee died and has now left the screen.";
 const BEE_ESCAPED = "The bee escaped beyond the end of the screen.";
@@ -26,19 +28,41 @@ const BEE_DROP_SPEED = 15;          // vertical drop speed of a dead bee
 const BEE_TOUCH_SENSITIVITY = 1.5;  // multiplier of radius
 const BEES_PER_LEVEL = 10;          // number of kills before speed increases
 
+const BEE_COLOUR = "#fce94f";
+const BEE_OUTLINE_COLOUR = "#2e3436";
+const BEE_OUTLINE_THICKNESS = 10;
+const BEE_STRIPE_OFFSET = 10;
+
+// Text
 const TEXT_FONT = "40px Source Code Pro";
 const TEXT_COLOUR = "#eeeeec";
+
 const START_TEXT = "Click to start touching bees!";
-const START_TEXT_NW_CORNER = new Point(140, 210)
+const START_TEXT_NW_CORNER = new Point(140, 210);
+
+const PAUSE_TEXT = "The game is paused.";
+const PAUSE_TEXT_NW_CORNER = new Point(280, 210);
+
 const END_TEXT1 = "Final score: ";
 const END_TEXT1_NW_CORNER = new Point(300,180);
 const END_TEXT2 = "Click to play again!";
 const END_TEXT2_NW_CORNER = new Point(250,END_TEXT1_NW_CORNER.y + 60);
 
-const BEE_COLOUR = "#fce94f";
-const BEE_OUTLINE_COLOUR = "#2e3436";
-const BEE_OUTLINE_THICKNESS = 10;
-const BEE_STRIPE_OFFSET = 10;
+// Buttons
+const BUTTON_COLOUR = "#4e9a06";
+
+const LEFT_BUTTON_NW_CORNER = new Point(360,260);
+const LEFT_BUTTON_WIDTH = 100;
+const LEFT_BUTTON_HEIGHT = 100;
+const LEFT_RESUME_TOP = new Point(LEFT_BUTTON_NW_CORNER.x + 20,
+                                  LEFT_BUTTON_NW_CORNER.y + 20);
+const LEFT_RESUME_RIGHT = new Point(LEFT_BUTTON_NW_CORNER.x
+                                    + LEFT_BUTTON_WIDTH - 10,
+                                    LEFT_BUTTON_NW_CORNER.y
+                                    + (LEFT_BUTTON_HEIGHT / 2));
+const LEFT_RESUME_BOTTOM = new Point(LEFT_BUTTON_NW_CORNER.x + 20,
+                                     LEFT_BUTTON_NW_CORNER.y
+                                     + LEFT_BUTTON_HEIGHT - 20);
 
 
 /* HELPER FUNCTIONS */
@@ -69,6 +93,7 @@ const GAME = {
     level: 1,
     highScore: localStorage.getItem(HIGH_SCORE_STRING),
     gameOver: true,
+    paused: false,
     startedPlaying: false,
     counter: 0,
 
@@ -78,9 +103,11 @@ const GAME = {
         this.playInterval = setInterval(
             (function(self) {
                 return function() {
-                    if (! self.gameOver) {
+                    if (! self.gameOver && ! self.paused) {
                         self.update();
                         self.draw();
+                    } else if (self.paused) {
+                        self.pausegame();
                     } else if (self.startedPlaying) {
                         self.endgame();
                     }
@@ -159,6 +186,29 @@ const GAME = {
                          START_TEXT_NW_CORNER.y);
     },
 
+    // Display the pause screen.
+    pausegame: function() {
+        this.clear();
+        CONTEXT.fillStyle = TEXT_COLOUR;
+        CONTEXT.font = TEXT_FONT;
+        CONTEXT.fillText(PAUSE_TEXT,
+                         PAUSE_TEXT_NW_CORNER.x,
+                         PAUSE_TEXT_NW_CORNER.y);
+
+        CONTEXT.fillStyle = BUTTON_COLOUR;
+        CONTEXT.fillRect(LEFT_BUTTON_NW_CORNER.x,
+                         LEFT_BUTTON_NW_CORNER.y,
+                         LEFT_BUTTON_WIDTH,
+                         LEFT_BUTTON_HEIGHT);
+
+        CONTEXT.fillStyle = TEXT_COLOUR;
+        CONTEXT.beginPath();
+        CONTEXT.moveTo(LEFT_RESUME_TOP.x, LEFT_RESUME_TOP.y);
+        CONTEXT.lineTo(LEFT_RESUME_RIGHT.x, LEFT_RESUME_RIGHT.y);
+        CONTEXT.lineTo(LEFT_RESUME_BOTTOM.x, LEFT_RESUME_BOTTOM.y);
+        CONTEXT.fill();
+    },
+
     // Display the end of game screen.
     endgame: function() {
         this.clear();
@@ -234,11 +284,13 @@ function Bee() {
         CONTEXT.stroke();
 
         CONTEXT.lineWidth = BEE_OUTLINE_THICKNESS;
+        CONTEXT.beginPath();
         CONTEXT.moveTo(this.centre.x - BEE_STRIPE_OFFSET,
                        this.centre.y - BEE_RADIUS);
         CONTEXT.lineTo(this.centre.x - BEE_STRIPE_OFFSET,
                        this.centre.y + BEE_RADIUS);
         CONTEXT.stroke();
+        CONTEXT.beginPath();
         CONTEXT.moveTo(this.centre.x + BEE_STRIPE_OFFSET,
                        this.centre.y - BEE_RADIUS);
         CONTEXT.lineTo(this.centre.x + BEE_STRIPE_OFFSET,
